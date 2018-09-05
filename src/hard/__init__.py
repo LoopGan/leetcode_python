57987b88dcb8528281adcea15c0f2a1ded47f24f
LoopGan <ganwei4955@gmail.com>
2018-09-05 15:31:44 +0800

    233. Number of Digit One

---
+++ src/hard/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    @author: LoopGan
+    @contact: ganwei4955@gamil.com
+    @time: 9/5/2018 1:19 PM
+"""
+
+if __name__ == "__main__":
+    print("hello imp")
+
+
+
---
+++ src/hard/number_of_digit_one.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    @author: LoopGan
+    @contact: ganwei4955@gamil.com
+    @time: 9/5/2018 1:20 PM
+"""
+
+
+class Solution:
+    def countDigitOne(self, n):
+        result = 0
+        for i in range(1, n + 1):
+            result += str(i).count('1')
+        return result
+
+
+if __name__ == "__main__":
+    n = 824883294
+    ss = Solution()
+    print(ss.countDigitOne(n))
+    print("hello imp")
